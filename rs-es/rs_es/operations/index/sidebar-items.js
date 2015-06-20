1f73cc637e0fbe571ecd62b67c47a6cd6d51ba7c
Ben Ashford <benashford@users.noreply.github.com>
2015-06-20 23:52:28 +0100

    Update for 0.1.12 documentation

--- rs-es/rs_es/operations/index/sidebar-items.js
+++ rs-es/rs_es/operations/index/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["IndexOperation","An indexing operation"],["IndexResult","The result of an index operation"]],"enum":[["OpType","Values for the op_type option"]]});
+initSidebarItems({"enum":[["OpType","Values for the op_type option"]],"struct":[["IndexOperation","An indexing operation"],["IndexResult","The result of an index operation"]]});
