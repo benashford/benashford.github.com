5e7a8797fe9089a5d896c214086a483deafe8d55
Ben Ashford <benashford@users.noreply.github.com>
2015-07-05 10:44:01 +0100

    Documentation updated for aggregations

--- rs-es/implementors/core/fmt/trait.Display.js
+++ rs-es/implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rs_es'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='rs_es/error/enum.EsError.html' title='rs_es::error::EsError'>EsError</a>","<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='rs_es/operations/common/struct.Options.html' title='rs_es::operations::common::Options'>Options</a>&lt;'a&gt;",];
+implementors['rs_es'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='rs_es/error/enum.EsError.html' title='rs_es::error::EsError'>EsError</a>","impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='rs_es/operations/common/struct.Options.html' title='rs_es::operations::common::Options'>Options</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- rs-es/implementors/rustc_serialize/serialize/trait.Decodable.js
+++ rs-es/implementors/rustc_serialize/serialize/trait.Decodable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rs_es'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Decodable.html' title='rustc_serialize::serialize::Decodable'>Decodable</a> for <a class='struct' href='rs_es/operations/struct.ShardCountResult.html' title='rs_es::operations::ShardCountResult'>ShardCountResult</a>",];
+implementors['rs_es'] = ["impl <a class='trait' href='http://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Decodable.html' title='rustc_serialize::serialize::Decodable'>Decodable</a> for <a class='struct' href='rs_es/operations/struct.ShardCountResult.html' title='rs_es::operations::ShardCountResult'>ShardCountResult</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
